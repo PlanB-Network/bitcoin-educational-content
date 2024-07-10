732832fb0e6279eaf7049efca69aa145f0a7b6f1
Asi0Flammeus <asi0.flammeus@pm.me>
2024-07-10 16:11:06 +0200

    py script, get supported languages

--- scripts/auto-translate/update-content-translation.py
+++ scripts/auto-translate/update-content-translation.py
@@ -9,8 +9,31 @@ def prepare_git_branch():
     print("New branch updated and ready to proceed!")
 
 
+def get_supported_languages():
+    """Returns an array of strings with the names of all Markdown (*.md) files in the directory, excluding 'en.md'."""
+
+    directory = "../../courses/btc101/"
+    supported_languages = []
+
+    if os.path.exists(directory):
+        for filename in os.listdir(directory):
+            if filename.endswith(".md") and filename != "en.md":
+                supported_languages.append(filename.split(".")[0])
+
+    else:
+        print("Directory does not exist.")
+
+    return supported_languages
+
+
+# Example usage:
+
+
 def main():
-#    prepare_git_branch()
+    # prepare_git_branch()
+    languages = get_supported_languages()
+    print(languages)
+    print("So far so good!")
 
 
 if __name__ == "__main__":

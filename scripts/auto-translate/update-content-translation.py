f4b42c9eae38ca0c886188ec40e85ef127088a70
Asi0Flammeus <asi0.flammeus@pm.me>
2024-07-10 17:55:30 +0200

    py script, copy to LLM-Translator for translation to en

--- scripts/auto-translate/update-content-translation.py
+++ scripts/auto-translate/update-content-translation.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from pathlib import Path
 
 
@@ -25,7 +26,6 @@ def get_supported_languages():
 
     return supported_languages
 
-
 def content_exist(filenames, lang):
     return any(f.endswith(f"{lang}.md") or f.endswith(f"{lang}.yml") for f in filenames)
     
@@ -48,21 +48,39 @@ def create_txt_to_en_from(lang):
                         file_written = True
 
     if not file_written:
+        os.remove(output_file)
         print(f"No need to translate from {lang}")
 
+def copy_from_repo_to_LLM_Translator(lang):
+    input_list_path = f"./translate-to-en/{lang}.txt"
+    destination_base_path = f"../../../LLM-Translator/inputs/pbn-from-{lang}-to-en/"
+
+    if not os.path.exists(input_list_path):
+        print(f"No file list found for language '{lang}'.")
+    else:
+        os.makedirs(destination_base_path, exist_ok=True)
+        with open(input_list_path, 'r') as file:
+            for line in file:
+                source_path = line.strip()
+                if not source_path:
+                    continue
+                new_filename = source_path.lstrip('./').replace('/', '_')
+                new_filename = new_filename.rstrip('_')
+
+                destination_file_path = os.path.join(destination_base_path, new_filename)
+
+                try:
+                    shutil.copy(source_path, destination_file_path)
+                    print(f"Copied and renamed '{source_path}' to '{destination_file_path}'")
+                except Exception as e:
+                    print(f"Error copying '{source_path}': {str(e)}")
 
 def main():
     # prepare_git_branch()
     languages = get_supported_languages()
-    print(languages)
-    create_txt_to_en_from("fr")
-    output_file_path = "./translate-to-en/fr.txt"
-    if os.path.exists(output_file_path):
-        with open(output_file_path, 'r') as file:
-            content = file.read()
-            print(content)
-    else:
-        print("No need to translate from fr.") 
+    for lang in languages:
+        create_txt_to_en_from(lang)
+        copy_from_repo_to_LLM_Translator(lang)
 
     print("So far so good!")
 

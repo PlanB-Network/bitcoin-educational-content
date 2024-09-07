a3df7ead7b6d6d89509fb267a19ef7e5922b98f7
Asi0Flammeus <asi0.flammeus@pm.me>
2024-09-07 18:35:35 +0200

    proofreading, script for getting reward for any content

--- scripts/proofreading-metadata/evaluate_proofreading_reward.py
+++ scripts/proofreading-metadata/evaluate_proofreading_reward.py
@@ -5,42 +5,6 @@ from proofreading import *
 from update_proofreading_metadata import *
 
 
-def get_subfolder_choices(root_directory):
-    subfolders = [
-        dirname for dirname in os.listdir(root_directory)
-        if os.path.isdir(os.path.join(root_directory, dirname)) and not dirname.startswith('.')
-    ]
-    return sorted(subfolders)
-
-def has_specific_file(subfolder_path, specific_files):
-    for file_name in os.listdir(subfolder_path):
-        if file_name in specific_files:
-            return True
-    return False
-
-def ask_for_subfolder(root_directory, specific_files):
-    while True:
-       subfolders = get_subfolder_choices(root_directory)
-        if not subfolders:
-            print(f"No subfolders found in {root_directory}.")
-            return None
-        question = [
-            inquirer.List('subfolder',
-                          message=f"Select a subfolder in {root_directory}",
-                          choices=subfolders,
-                          carousel=True,
-                          ),
-        ]
-        answers = inquirer.prompt(question)
-        selected_subfolder = answers['subfolder']
-        selected_subfolder_path = os.path.join(root_directory, selected_subfolder)
-        if has_specific_file(selected_subfolder_path, specific_files):
-            print(f"The subfolder '{selected_subfolder}' contains a specific file.")
-            return selected_subfolder_path
-        else:
-            print(f"Recursing into subfolder: {selected_subfolder}")
-            return ask_for_subfolder(selected_subfolder_path, specific_files)
-
 def select_language(content_path):
     content_languages = get_language_list_for_content(content_path)
     question = [
@@ -60,15 +24,50 @@ def main():
     root_directory = '../../'
 
     while True:
-        selected_subfolder_path = ask_for_subfolder(root_directory, specific_files)
-        if selected_subfolder_path:
-            print(f"Selected subfolder: {selected_subfolder_path}")
-            add_new_contribution_to(selected_subfolder_path)
-        else:
-            print("No valid subfolder with specific files was found.")
+        question = "Do you want to know the proofreading reward of a content? "
+        user_response = ask_yes_no_question(question)
+        if user_response == 'y':
+            selected_subfolder_path = ask_for_subfolder(root_directory, specific_files)
+            if selected_subfolder_path:
+                print(f"Selected subfolder: {selected_subfolder_path}")
+                
+                file_path = get_existing_file_path(selected_subfolder_path,
+                                                   specific_files) 
+                content_name = os.path.basename(selected_subfolder_path)
+                data = get_yml_content(file_path)
+                custom_language_question = "Do you want to evaluate for custom language factors?"
+                custom_language = ask_yes_no_question(custom_language_question)
+                if custom_language == 'y':
+                    selected_language = "Custom language"
+                    language_factor = float(input("What's the language factor? "))
+                    urgency = float(input("What's the urgency? "))
+
+                    words = get_words(file_path)
+                    difficulty_factor = get_difficulty_factor(data)
+
+
+                    proofread_iteration = 0
+                    reward = compute_reward(words, difficulty_factor, language_factor, urgency, BASE_FEE, proofread_iteration)
+                    
+                else:
+                    selected_language = select_language(selected_subfolder_path)
+                    proofread_iteration = get_proofreading_state(data, selected_language)  
+                    reward = evaluate_proofreading_reward(file_path, selected_language)
+                print()
+                print(f"For {content_name}:")
+                print(f"The proofreading reward is: {reward} sats")
+                left = 3 - proofread_iteration
+                print(f"{left} proofreading(s) are/is missing for {selected_language}")
+                print()
+
+                    
+            else:
+                print("No valid subfolder with specific files was found.")
+        elif user_response == 'n':
+            break
         
-        ask_yes_no_question()
-        select_language(selected_subfolder_path)
+        
+            
         
 
 if __name__ == "__main__":

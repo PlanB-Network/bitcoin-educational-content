706e902d231f413b5246e7bd7af950c5221a729c
PBN Repo Maintainers <bec@planb.network>
2025-07-19 03:00:10 +0000

    remove scu202 from frozenset

--- scripts/auto-translate/batch-translate/update_course_and_quizz_translation.py
+++ scripts/auto-translate/batch-translate/update_course_and_quizz_translation.py
@@ -12,7 +12,7 @@ class TranslationConfig:
     """Configuration class for translation settings."""
     script_dir: Path = Path(__file__).resolve().parent
     project_root: Path = script_dir.parent.parent.parent
-    excluded_subfolders: Set[str] = frozenset({'biz221' , 'biz205' , 'scu202' , 'pro202' , 'btc302' })  
+    excluded_subfolders: Set[str] = frozenset({'biz221' , 'biz205' , 'pro202' , 'btc302' })  
     to_en_dir: Path = script_dir / "translate-to-en"
     from_en_dir: Path = script_dir / "translate-from-en"
 
